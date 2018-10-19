abad08caaef552cb618294978bef39ce8fb09e7f
Hugo-Olivier Monette <ak89420@ens.etsmtl.ca>
2018-10-18 23:03:41 -0400

    Demo Version | Dynamic header WIP

--- templates/default_cls.js
+++ templates/default_cls.js
@@ -1,1 +1,14 @@
-export default ``;
+module.exports = (fileName, userName, currentTime) => `/**
+ * @File Name          : ${fileName}
+ * @Description        : 
+ * @Author             : ${userName}
+ * @Group              : 
+ * @Last Modified by   : 
+ * @Last Modified time : ${currentTime}
+ * @Modification Log   : 
+ *-------------------------------------------------------------------------------------
+ * Ver       	   Date           Author      		   Modification
+ *-------------------------------------------------------------------------------------
+ * 1.0       ${currentTime}    ${userName}  Initial Version
+ **/
+`
