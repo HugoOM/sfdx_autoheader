0e272d1a66ddc37903aecebcd4eddc92f0809f91
Hugo-Olivier Monette <ak89420@ens.etsmtl.ca>
2018-10-18 23:03:41 -0400

    Demo Version | Dynamic header WIP

--- extension.js
+++ extension.js
@@ -1,24 +1,28 @@
 const {
-  commands,
   Position,
-  languages,
   workspace,
   TextEdit
 } = require('vscode');
+const defaultTemplate = require('./templates/default_cls.js')
 
-function activate(context) {
+function activate() {
   workspace.onWillSaveTextDocument(event => {
     if (isLineAComment(event.document.lineAt(0).text)) return;
 
-    event.waitUntil(prependFileHeader());
+    event.waitUntil(prependFileHeader(event.document));
   });
 }
 
 exports.activate = activate;
 exports.deactivate = function () {};
 
-function prependFileHeader() {
-  return Promise.resolve([TextEdit.insert(new Position(0, 0), `//Test String \n`)])
+function prependFileHeader(document) {
+  return Promise.resolve([
+    TextEdit.insert(
+      new Position(0, 0),
+      defaultTemplate(document.fileName.split('/')[document.fileName.split('/').length - 1], 'hmonette@deloitte.ca', 'placeholder')
+    )
+  ])
 }
 
 function isLineAComment(textContent) {
