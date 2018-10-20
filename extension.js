5f2e74242a3ea28d9fe324f3cbcca9af984fc3ca
Hugo-Olivier Monette <ak89420@ens.etsmtl.ca>
2018-10-20 15:31:15 -0400

    WIP Header Text Update

--- extension.js
+++ extension.js
@@ -1,28 +1,36 @@
 const {
   Position,
   workspace,
-  TextEdit
+  TextEdit,
+  Disposable
 } = require('vscode');
 
 const defaultTemplate = require('./templates/default_cls.js')
 
-function activate() {
-  workspace.onWillSaveTextDocument(event => {
+const disposables = []
+
+function activate(context) {
+  const preSaveHookListener = workspace.onWillSaveTextDocument(event => {
+    if (!isLanguageSFDC(event.document)) return;
     if (isLineAComment(event.document.lineAt(0).text)) return;
 
     event.waitUntil(prependFileHeader(event.document));
-  });
+    // event.waitUntil(updateLastModifiedBy(event.document.getText()));
+  })
+
+  context.subscriptions.push(preSaveHookListener);
 }
 
 exports.activate = activate;
-//TODO: Expose listener in "Global Scope" and Dispose upon deactivation
-exports.deactivate = function () {};
+exports.deactivate = function () {
+  // disposables.forEach(disposable => disposable.dispose())
+};
 
 async function prependFileHeader(document) {
   return [
     TextEdit.insert(
       new Position(0, 0),
-      defaultTemplate(document.fileName.split(/\/|\\/g).pop(), 'hmonette@deloitte.ca', 'placeholder')
+      defaultTemplate(document.fileName.split(/\/|\\/g).pop(), 'hmonette@deloitte.ca', getHeaderFormattedDateTime())
     )
   ]
 }
@@ -30,4 +38,18 @@ async function prependFileHeader(document) {
 function isLineAComment(textContent) {
   const re = /(^\/\/)|(^\/\*)/g;
   return !!textContent.trim().match(re);
+}
+
+function isLanguageSFDC(document) {
+  return document.languageId === "apex"
+}
+
+function getHeaderFormattedDateTime() {
+  const currentDate = new Date(Date.now());
+  return currentDate.toLocaleString()
+}
+
+async function updateLastModifiedBy(fileContent) {
+  const re = /(?<=\s*\*\s*@Last\s*Modified\s*by\s*:\s*).*$/gim;
+  headerContent.replace(re, 'bamboo@lit.com')
 }
