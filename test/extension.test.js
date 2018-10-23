f17b29abfc3e083a311a15118660688566402fee
Hugo-Olivier Monette <ak89420@ens.etsmtl.ca>
2018-10-22 22:07:51 -0400

    Travis hook testing

--- test/extension.test.js
+++ test/extension.test.js
@@ -18,7 +18,7 @@ suite("Extension Tests", function () {
 
     // Defines a Mocha unit test
     test("Something 1", function () {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+        assert.equal(-1, [1, 2, 3].indexOf(5))
+        assert.equal(-1, [1, 2, 3].indexOf(0))
     });
 });
